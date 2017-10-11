03ace8e4153dba770fb691d4be3e238cf621cd12
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-11 17:51:36 -0400

    routed kitten, added a heading, defined @kitten_url to take the dynamic value stored in params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,10 @@ class PagesController < ApplicationController
   def contest
     @header = "This is our contest page"
   end
+
+  def kitten
+    @header = "This is our kitten page"
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
