1a8e5c0e6691318402b188efa9c66fc95392d1de
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-11 17:38:13 -0400

    added the header to each page and replaced the text inside with lorem ipsum

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "This is the Welcome Page"
   end
 
   def about
+    @header = "This is the About page"
   end
 
   def contest
+    @header = "This is our contest page"
   end
 end
