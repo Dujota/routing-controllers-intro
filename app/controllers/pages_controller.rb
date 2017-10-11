9dd570ef90fa3fa3542c8498e534c88be66271eb
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-11 18:04:31 -0400

    setup the before filter for set_kitten_url and all works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens] # this is the same as saying include the set_kitten_url in the methods named under only when run
 
   def welcome
     @header = "This is the Welcome Page"
@@ -14,6 +15,15 @@ class PagesController < ApplicationController
 
   def kitten
     @header = "This is our kitten page"
+    # set_kitten_url  -- included in the top
+  end
+
+  def kittens
+    @header = "There are many kittens here!"
+    # set_kitten_url -- included in the top
+  end
+
+  def set_kitten_url # "set_" defines a method that fetches a picture based on the size of the wildcard value stored in params
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
